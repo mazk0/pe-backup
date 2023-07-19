5d38806042a9041f0cb8c4508759e1d316b1ef1a
Mattias Makra <mattias@makra.org>
2023-07-19 09:26:38 +0200

    Only initiate http client once.

--- BackupService.cs
+++ BackupService.cs
@@ -2,10 +2,11 @@ namespace PEBackup;
 
 public static class BackupService
 {
+    private static readonly HttpClient Client = new();
+    
     public static async Task Backup(string sourceUrl, string destinationPath)
     {
-        var client = new HttpClient();
-        var responseStream = await client.GetStreamAsync(sourceUrl);
+        await using Stream responseStream = await Client.GetStreamAsync(sourceUrl);
         await using Stream streamToWriteTo = File.Open(destinationPath, FileMode.Create);
         await responseStream.CopyToAsync(streamToWriteTo);
     }
